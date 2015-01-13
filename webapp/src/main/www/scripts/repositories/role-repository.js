18e1a6045b922b81b98c9eefb33e699e3df38c62
dianw <dian.aditya@meruvian.org>
2015-01-14 03:27:15 +0700

    Cleanup project

--- webapp/src/main/www/scripts/repositories/role-repository.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('yamaApp')
-  .factory('RoleRepository', function ($injector, RoleModel) {
-    var BaseRepository = $injector.get('BaseRepository');
-    return new BaseRepository({name: 'RoleRepository', model: RoleModel});
-  });
