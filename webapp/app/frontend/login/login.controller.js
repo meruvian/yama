3bd61a09b602bec695cb777d5307507122edd4a3
dianw <dian.aditya@meruvian.org>
2015-06-02 14:54:12 +0700

    Resolves #7: Upgrade UI to AdminLTE 2 with Bower

--- webapp/app/frontend/login/login.controller.js
+++ webapp/app/frontend/login/login.controller.js
@@ -1,40 +1,10 @@
 'use strict';
 
-angular.module('yamaApp').controller('LoginCtrl', function ($scope, $state, $rootScope, $location, OAuth, Users) {
-	$scope.isAuthenticated = true;
-
-	$scope.go = function() {
-		if ($rootScope.$state.toState) {
-			var toState = $rootScope.$state.toState;
-			delete $rootScope.$state.toState;
-
-			$state.go(toState.name, toState.params);
-		} else {
-			$state.go('main');
-		}
-	};
-
-	if (OAuth.isAuthenticated()) {
-		$scope.go();
-	} else {
-		Users.one('me').get().then(function() {
-			OAuth.getAccessTokenFromCode();
-		});
-	}
-
-	$scope.login = function(user) {
-		$scope.isAuthenticated = true;
-
-		OAuth.getAccessToken(user).then(function() {
-			$scope.go();
-		}, function() {
-			$scope.isAuthenticated = false;
-		});
-	};
+angular.module('yamaApp').controller('LoginCtrl', function () {
+	// do nothing
 }).controller('LogoutCtrl', function($state, $http, YamaOAuth) {
 	$http.get('/auth/logout').success(function() {
 		YamaOAuth.logout();
-
 		$state.go('main');
 	});
 });
--- webapp/app/frontend/login/login.route.js
+++ webapp/app/frontend/login/login.route.js
@@ -3,7 +3,6 @@
 angular.module('yamaApp').config(function ($stateProvider) {
 	$stateProvider.state('login', {
 		url: '/login',
-		templateUrl: 'frontend/login/login.html',
 		controller: 'LoginCtrl'
 	});
 
