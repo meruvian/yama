f994cb46395d632e6eeeb9490d914c1be8f1b059
Dian Aditya <dian.aditya@meruvian.org>
2016-06-12 01:51:30 +0700

    Fix #39: Remember-Me returned 403 Forbidden

--- webapi/src/main/java/org/meruvian/yama/webapi/config/SecurityConfig.java
+++ webapi/src/main/java/org/meruvian/yama/webapi/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/").permitAll()
-				.antMatchers("/oauth/authorize").fullyAuthenticated()
+				.antMatchers("/oauth/authorize").authenticated()
 				.and()
 			.formLogin()
 				.loginPage(LOGIN_PAGE_URL)
